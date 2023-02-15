0a39c9a8fb6a43cdf86c1c976396ee3da5304e5b
Jean De Montigny <njd233@newcastle.ac.uk>
2023-02-15 16:10:32 +0000

    add import for bxr_* and brw_spikeinterface files

--- beaf/__init__.py
+++ beaf/__init__.py
@@ -2,4 +2,7 @@
 
 from .brw_recording import *
 from .brw_experiment_settings import *
+from .bxr_experiment_settings import *
+from .bxr_recording import *
+from .brw_spikeinterface import *
 from .utils import *
